0dbc492756f19e5e9b0c4b51d07fed11dfff1df4
carterscott <98565111+carterscott344@users.noreply.github.com>
2024-10-30 18:25:26 -0500

    made a few additions. havent done anything for React

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let cartCount = 0;
 
-function updateCartDisplay(amount) {
-    cartCount += amount; 
+function updateCartDisplay(idNumber) {
+
+    increment = getItemCount(idNumber); 
+    if (isNaN(increment)) {
+        cartCount = cartCount;
+    } else {
+        cartCount += increment;
+    }
     let cartCountDisplay;
      
     if (cartCount <= 0) {
         cartCountDisplay = ''; 
+        cartCount = 0;
     } else {
         cartCountDisplay = cartCount;  
     }
@@ -19,4 +26,33 @@ function updateCartDisplay(amount) {
         displayElement.style.display = 'inline-block';  
     }
 }
-updateCartDisplay(0) //start at ''
+
+const itemCounts = {};
+
+function updateCount(idNumber, amount) {
+    if (!itemCounts[idNumber]) {
+        itemCounts[idNumber] = 0;
+    }    
+    itemCounts[idNumber] += amount;
+ 
+    if (itemCounts[idNumber] <= 0) {
+        itemCounts[idNumber] = 0;
+    }
+
+    const displayElement = document.getElementById('item' + idNumber + 'Count');
+    if (itemCounts[idNumber] > 0) {
+        displayElement.innerHTML = itemCounts[idNumber];
+    } else {
+        displayElement.innerHTML = "<br />";
+    }
+
+}
+
+
+function getItemCount(idNumber){
+    return parseInt(document.getElementById('item' + idNumber +'Count').innerText, 10);
+}
+
+updateCartDisplay(1);
+
+
